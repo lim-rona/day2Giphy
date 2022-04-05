7b291a102e0b4b6af7365a556811116e4b917bbd
Lim Rona <limrona@Lims-MacBook-Pro.local>
2022-04-05 11:04:48 +0800

    initial commit

---
+++ src/main/java/vttp2022/paf/day2Giphy/Day2GiphyApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.paf.day2Giphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2GiphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2GiphyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/paf/day2Giphy/Day2GiphyApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.paf.day2Giphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2GiphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
